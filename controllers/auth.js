eefb472f7aec6902302ebd5fd3e50601b92634fe
SantiagoO <santiagoo@digitalware.com.co>
2021-11-29 15:07:50 -0500

    Fin seccion 15 - Arreglos para subir img front

--- controllers/auth.js
+++ controllers/auth.js
@@ -96,9 +96,13 @@ const renewToken = async (req, res = response) => {
     //Generar TOKEN -JWT
     const token = await generarJWT(uid);
 
+    //Obtener Usuario
+    const usuario = await Usuario.findById(uid); 
+
     res.json({
         ok: true,
-        token
+        token,
+        usuario
     })
 }
 
